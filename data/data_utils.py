dc19d2da19eb41fa3185e680ed1e656137ba92b1
albornet <alban.bornet@gmail.com>
2022-08-22 17:51:16 +0200

    corrected the script that unzip the pickle and generate the json file

--- data/data_utils.py
+++ data/data_utils.py
@@ -2,7 +2,7 @@ import os
 import json
 import pandas as pd
 import zipfile
-import shutil
+
 
 def unzip_data(zip_path, dest_dir):
     with zipfile.ZipFile(zip_path, 'r') as z:
@@ -10,6 +10,7 @@ def unzip_data(zip_path, dest_dir):
 
 
 def pickle_to_json(pickle_dir, json_dir):
+    os.makedirs(json_dir, exist_ok=True)
     for pkl_path in os.listdir(pickle_dir):
         if '.pickle' in pkl_path:
             print(f'Writing {pkl_path} to a json file')
@@ -33,11 +34,17 @@ def pickle_to_json(pickle_dir, json_dir):
 
 
 if __name__ == "__main__":
+    
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    zip_file_path = os.path.join(cwd, 'pickle.zip')
+    pickle_dir = os.path.join(cwd, 'pickle')
+    json_dir = os.path.join(cwd, 'json')
 
-    zip_file_path = './json.zip'
-    pickle_dir = './pickle'
-    json_dir = './json'
-
-    unzip_data(zip_file_path, './')
-    shutil.rmtree(zip_file_path)
+    unzip_data(zip_file_path, cwd)
     pickle_to_json(pickle_dir, json_dir)
+
+    if input('Delete zip file? (y/n): ') == 'y':
+        print('Deleting the zip file.')
+        os.remove(zip_file_path)
+    else:
+        print('Not deleting the zip file.')
